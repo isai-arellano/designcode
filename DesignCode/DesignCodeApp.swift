d3837e80f19fc5d859f16336938b08db115647db
Isaí Arellano <isai.arellano@outlook.com>
2022-05-27 17:27:23 -0500

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Isaí on 27/05/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
