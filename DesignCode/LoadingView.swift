c3b8c4279ebc7e708393ffae8a53da3a9012f251
Isaí Arellano <isai.arellano@outlook.com>
2022-07-07 20:48:16 -0500

    SE AGREGO ANIMACION Y LOGIN CON LIBRERIA LOTTIE

---
+++ DesignCode/LoadingView.swift
@@ -0,0 +1,23 @@
+//
+//  LoadingView.swift
+//  DesignCode
+//
+//  Created by Isaí on 07/07/22.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        VStack {
+            LottieView(filename: "loading")
+                .frame(width: 200, height: 200)
+        }
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
